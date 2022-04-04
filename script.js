08d0012596fb0f0a9e67d4133825307cfae4e62e
Jhuliana <jholeanasantos@gmail.com>
2022-04-04 02:08:18 -0300

    Projeto com o javascript finalizado

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ function finalizar_pedido (tipo1, tipo2, tipo3) {
         retirar.classList.remove('display-none')
     }
 }
+
+function realizarPedido(tipo1, tipo2, tipo3) {
+    let texto;
+    let total;
+
+    
+    const sobremesa = document.querySelector(`${tipo1} .select h2`).innerHTML;
+    const bebida  = document.querySelector(`${tipo2} .select h2`).innerHTML;
+    const prato_principal = document.querySelector(`${tipo3} .select h2`).innerHTML;
+    const valor_sobremesa = document.querySelector(`${tipo1} .select h4`).innerHTML.slice(3).replace(",", ".");
+    const valor_bebida = document.querySelector(`${tipo2} .select h4`).innerHTML.slice(3).replace(",", ".");
+    const valor_prato = document.querySelector(`${tipo3} .select h4`).innerHTML.slice(3).replace(",", ".");
+
+    total =  Number(valor_prato) + Number(valor_bebida) + Number(valor_sobremesa);
+    total = total.toFixed(2)
+    texto = encodeURIComponent(`Olá, gostaria de fazer o pedido:
+- Prato: ${prato_principal}
+- Bebida: ${bebida}
+- Sobremesa: ${sobremesa}
+Total: R$ ${total}`) 
+    
+    window.open("https://wa.me/55061998234001?text=" + texto)  
+}
